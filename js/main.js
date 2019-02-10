1b488562c78fcda05eea6f37e3f28f3c891751d1
baylo014 <baylo014@umn.edu>
2019-02-09 18:59:53 -0600

    Text-Preview Javascript for modals added

--- js/main.js
+++ js/main.js
@@ -13,6 +13,28 @@ var pyClose = document.getElementsByClassName("close")[0];
 var javaClose = document.getElementsByClassName("close")[1];
 var cClose = document.getElementsByClassName("close")[2];
 var cppClose = document.getElementsByClassName("close")[3];
+/* https://stackoverflow.com/questions/1199352/smart-way-to-shorten-long-strings-with-javascript */
+/* This truc function was suggested by user "KooiInc" on StackOverflow */
+String.prototype.trunc =
+  function (n, useWordBoundary) {
+    if (this.length <= n) {
+      return this;
+    }
+    var subString = this.substr(0, n - 1);
+    return (useWordBoundary ?
+      subString.substr(0, subString.lastIndexOf(' ')) :
+      subString) + "&hellip;";
+  };
+
+var pyPre = document.getElementsByClassName("pyText")[0].textContent;
+document.getElementById("pyPreview").innerHTML = pyPre.trunc(400, true);
+var javaPre = document.getElementsByClassName("javaText")[0].textContent;
+document.getElementById("javaPreview").innerHTML = javaPre.trunc(400, true);
+var cPre = document.getElementsByClassName("cText")[0].textContent;
+document.getElementById("cPreview").innerHTML = cPre.trunc(400, true);
+var cppPre = document.getElementsByClassName("c++Text")[0].textContent;
+document.getElementById("c++Preview").innerHTML = cppPre.trunc(400, true);
+
 /* On click functions to open the modal */
 pyBtn.onclick = function () {
   pyModal.style.display = "block";
